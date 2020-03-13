f4b474335e6d4edae1f282c7a0af4e15564c1c56
intpart-NhanIT <nhan.truong@interactivepartners.com.au>
2020-03-13 23:04:34 +0700

    fix

--- push.js
+++ push.js
@@ -1,16 +1,16 @@
 var push = require('web-push');
 let vapidKeys = {
-    publicKey: 'BBAQXM9gNxGSkaeYn4Tq27FTYdX8l52097mfettqHbiDd2fCmUwaiDoTFVScqJEljdU8K0PTGp9PeD3fW9eT2x8',
-    privateKey: 'SUUU_KK8d30vWQxqTxt7eWJI8zeIwm_FRVhiPnfvj4k'
+    publicKey: 'BNVusQ8KsdNOPHQtEDZCWCje2VrhZheGc33DWgJvvzTctOWDmVz3DgWKCWzS1N2WTWlhMfZ2xWdrGmmhZ831m_s',
+    privateKey: 'Assqq5WMpNl9os-X__OilFHvfWS9f-1nrTCmQZfJgso'
 }
 push.setVapidDetails('mailto:test@code.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
 let sub = {
-    endpoint: "https://fcm.googleapis.com/fcm/send/crsa2GefmB8:APA91bF4LVy6TfH5LMAfj7Q0RrB1poIP63y3WpJ-aiQa_LCwTQqzNnKxG5h9LboWbnBgfZ5yChVXhCGLYHRXRdT_gbrD7WvrnslPLk0cf1qAGD8gucdFe4UzX6GN8VwnA3K3dsOKisXJ",
+    endpoint: "https://fcm.googleapis.com/fcm/send/f9r-ZQsiZho:APA91bFy_6Or46EhHjy26hZstkKZ5xyUroC7-pzNo5kLVLliLv4C9X3pDhn42bvXRfYnbRAtw6mk2e8P5UA1apIFNvS1YyvhAXQUtCElUG6QUDl3woWV3SUhPEcIF6P_q9ENLkSqPsdx",
     expirationTime: null,
     keys: {
-        p256dh: "BAHyu-JRFwblhh9Dx4OCLagMBQG1AXrkAp4GCa4fvEt_7yvb6KDZzFhaWRNKec2uGReyY7wuyvZ_L91aQ4za8fU",
-        auth: "OKYZu5WsdlSbh6R2oKF7JA"
+        p256dh: "BDNlPmPhjqruCAnw2jB9FFKVI7KKXLJTCfyKu5hNWvT4SW9kdlaWYF05tv4segzrU762ypWGaecQ9AHuOS8e_OE",
+        auth: "PxLAttXU5eckuSwB8MbnZQ"
     }
 };
 
--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('push', function (e) {
     self.clients.matchAll().then(function (clientList) {
         return self.registration.showNotification('ServiceWorker Cookbook', {
-          body: "ANH NHAN DEP TRAI",
+          body: "ANH NHAN DEP TRAI1",
         });
   
       })
